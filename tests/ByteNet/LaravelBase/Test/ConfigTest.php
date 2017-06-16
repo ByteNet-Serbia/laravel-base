18db64fde25ea8d17c522a01d623536221120a33
Nikola Zeravcic <niks986@gmail.com>
2017-06-17 00:03:52 +0200

    Travis CI base setup (#1)
    * Travis CI - set solution for MySQL < 5.7.7
    
    About error info on: https://laravel-news.com/laravel-5-4-key-too-long-error
    
    * Extra test file 'ByteNet\LaravelBase\Test\ByteNet\LaravelBase\Test\ConfigTest'

---
+++ tests/ByteNet/LaravelBase/Test/ConfigTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace ByteNet\LaravelBase\Test\ByteNet\LaravelBase\Test;
+
+class ConfigTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * testAddPackagistRepository
+     */
+    public function testAddPackagistRepository()
+    {
+        $this->assertEquals(true, true);
+    }
+}
