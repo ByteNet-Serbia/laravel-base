09371dcc122f40f4dbf2cb27084d0e0d9c68cd89
Nikola Zeravcic <office@bytenet.rs>
2017-06-16 22:26:25 +0200

    Travis CI - set solution for MySQL < 5.7.7
    About error info on: https://laravel-news.com/laravel-5-4-key-too-long-error

---
+++ tests/AppServiceProvider.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\ServiceProvider;
+
+class AppServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+        // Travis CI - set solution for MySQL < 5.7.7 (https://laravel-news.com/laravel-5-4-key-too-long-error)
+        Schema::defaultStringLength(191);
+    }
+
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
